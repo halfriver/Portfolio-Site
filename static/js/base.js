487c134e494ac0bf10f5073acf2b9a2d12148116
halfriver <ohkawa2.0@gmail.com>
2020-12-08 18:41:11 -0500

    cv page skeleton

--- static/js/base.js
+++ static/js/base.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 
-  var collapseStart = 160;
+  var collapseStart = 120;
   var atTop = true;
   var navbarInitialHeight = $("nav").height();
 
   if ($(window).scrollTop() > collapseStart){
-    $('nav').attr('style', 'padding: 10px 0px !important;');
+    $('nav').attr('style', 'padding: 5px 0px !important;');
     $('nav').css('background-color', 'rgba(0, 0, 0, 1)');
   }
 
   $(window).scroll(function() {
     if ($(this).scrollTop() > collapseStart){
-      $('nav').attr('style', 'padding: 10px 0px !important;');
+      $('nav').attr('style', 'padding: 5px 0px !important;');
       $('nav').css('background-color', 'rgba(0, 0, 0, 1)');
       atTop = false;
     }
     else{
       if ($("nav").height() > navbarInitialHeight) {
-        $('nav').attr('style', 'padding: 20px 0px !important');
+        $('nav').attr('style', 'padding: 15px 0px !important');
         $('nav').css('background-color', 'rgba(0, 0, 0, 1)');
       } else {
-        $('nav').attr('style', 'padding: 20px 0px !important');
+        $('nav').attr('style', 'padding: 15px 0px !important');
         $('nav').css('background-color', 'rgba(0, 0, 0, .35)');
       }
       atTop = true;
