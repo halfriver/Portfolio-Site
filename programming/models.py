8b6777f37ef442359866e886f4844746c8b0a9f9
halfriver <ohkawa2.0@gmail.com>
2020-12-22 22:29:27 -0500

    card progress

--- programming/models.py
+++ programming/models.py
@@ -6,6 +6,7 @@ class Program(models.Model):
     html = models.CharField(max_length=30, null=True)
     code = models.CharField(max_length=30, null=True)
     demo = models.BooleanField(null=True)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.name
