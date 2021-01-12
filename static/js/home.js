ad8771ea194337e609dcd228d7f1fe97a8da2425
halfriver <ohkawa2.0@gmail.com>
2021-01-12 02:33:08 -0500

    progress

--- static/js/home.js
+++ static/js/home.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
   var m = new Date().getMonth();
   var d = new Date().getDate();
   if (m == 2 && d == 27) {
-    $("#art-pic").css("src", "static/media/birthdayfish.png").css("title", "It's my birthday today!")
+    $("#art-pic").css("src", "static/media/birthdayfish.png").attr("title", "It's my birthday today!");
   }
   else if (m == 11 && d < 27) {
-    $("#art-pic").css("src", "static/media/christmasfish.png").css("title", "Happy Holidays!")
+    $("#art-pic").css("src", "static/media/christmasfish.png").attr("title", "Happy Holidays!");
   }
   else if ( (m == 11 && d >= 27) || (m == 0 && d < 8) ) {
-    $("#art-pic").attr("src", "static/media/newyearsfish.png").css("title", "Happy New Year! あけおめ！")
+    $("#art-pic").attr("src", "static/media/newyearsfish.png").attr("title", "Happy New Year! あけおめ！");
   }
   else if (m == 9 && d == 31) {
-    $("#art-pic").attr("src", "static/media/halloweenfish.png").css("title", "Happy Halloween!")
+    $("#art-pic").attr("src", "static/media/halloweenfish.png").attr("title", "Happy Halloween!");
   }
 });
