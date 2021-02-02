9789cc924b5355e5d36dd98c67c3e1b4cd0e65c6
halfriver <ohkawa2.0@gmail.com>
2021-02-01 19:10:37 -0500

    launch hotfix

---
+++ myportsite/secrets.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'kyu=%(#3u20ldp8(oys%13lc@*!g^qs&5&xy)(mvipswlwkrp*'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
